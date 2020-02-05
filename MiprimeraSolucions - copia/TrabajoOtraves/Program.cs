04aa9730feeece906a9d9cf4612bae8c15f73904
kelvinju <kelvinjruvalcaba@gmail.com>
2020-02-05 15:08:45 -0600

    miprimerproyecto

---
+++ MiprimeraSolucions - copia/TrabajoOtraves/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoOtraves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("introduce un numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("introduce otro numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("la suma de los numeros introducidos es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
