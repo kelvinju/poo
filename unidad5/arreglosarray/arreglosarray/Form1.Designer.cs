dcf64c260d9786a5939eed5099e93186084becb1
kelvinju <kelvinjruvalcaba@gmail.com>
2020-04-01 21:24:49 -0600

    unidadCinco

---
+++ unidad5/arreglosarray/arreglosarray/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace arreglosarray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comnotas = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.texPro = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "cargar datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "notas";
+            // 
+            // comnotas
+            // 
+            this.comnotas.FormattingEnabled = true;
+            this.comnotas.Location = new System.Drawing.Point(81, 157);
+            this.comnotas.Name = "comnotas";
+            this.comnotas.Size = new System.Drawing.Size(121, 21);
+            this.comnotas.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Promedio";
+            // 
+            // texPro
+            // 
+            this.texPro.Location = new System.Drawing.Point(103, 211);
+            this.texPro.Name = "texPro";
+            this.texPro.Size = new System.Drawing.Size(76, 20);
+            this.texPro.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 299);
+            this.Controls.Add(this.texPro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comnotas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comnotas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox texPro;
+    }
+}
+
---
+++ unidad5/arreglosarray/arreglosarray/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arreglosarray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declaracion de las variables
+            int[] notas = new int[6];
+            //especificar las notas
+            notas[0] = 10;
+            notas[1] = 10;
+            notas[2] = 9;
+            notas[3] = 10;
+            notas[4] = 8;
+            notas[5] = 6;
+            int suma = 0;
+            double promedio;
+            //proceso
+            comnotas.Items.Clear();
+            //recorrer nuestro arreglo
+            for(int i =0; i<=5;i++)
+            {
+                //mostrar las notas de los arreglos en el combobox
+                comnotas.Items.Add(notas[i]);
+                suma = suma + notas[i];
+            }
+            promedio = suma / 6;
+            texPro.Text = Convert.ToString(promedio);
+        }
+    }
+}
